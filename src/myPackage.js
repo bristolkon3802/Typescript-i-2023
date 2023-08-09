8cfcfb40702a279334ffc3fcc1df288dfc5d9f99
bristolkon <bristolkon3802@gmail.com>
2023-08-09 18:00:09 +0900

    6.JSDoc 타입스크립트가 코멘트를 읽을 수 있음

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,19 @@
+//@ts-check
+/**
+ * 프로젝트를 초기화 한다.
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function inin(config) {
   return true;
 }
+/**
+ * 프로그램 종료
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
